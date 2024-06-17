a8cef1dbbbd9a024aeaf4ca6817dd2eaf9bf9efd
Jeremy Fowler <jembyfowler@gmail.com>
2024-06-17 13:52:30 -0600

    updating score of first gamer in array

--- app/services/GamersService.js
+++ app/services/GamersService.js
@@ -1,8 +1,13 @@
+import { AppState } from "../AppState.js";
 
 // NOTE do not export class definition for services, so no one can create a new Service
 class GamersService {
   increaseScore() {
     console.log('Increasing score, but from the service');
+    // NOTE You can access properties on the AppState by using dot notation 
+    const gamers = AppState.gamers
+    gamers[0].score++
+    console.log('here are the gamer score', gamers[0].score);
   }
 }
 
