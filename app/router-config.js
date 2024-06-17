876eb17eaf3326f71a8e538d81fe80fb3121949e
Jeremy Fowler <jembyfowler@gmail.com>
2024-06-17 12:26:58 -0600

    added notes

--- app/router-config.js
+++ app/router-config.js
@@ -4,9 +4,12 @@ import { HomeController } from "./controllers/HomeController.js";
 import { Router } from "./utils/Router.js";
 
 export const router = new Router([
+  // Use first object today for page load stuff
   {
     path: '',
+    // NOTE loads supplied Controllers when page loads, and makes them accessible to DOM 
     controllers: [GamersController],
+    // NOTE change to empty string so we do not inject HTML on page load
     view: ''
   },
   {
